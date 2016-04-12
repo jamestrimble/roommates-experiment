7b244d17224dc4d09fb52d33d7a875538d06ec60
jamestrimble <james.trimble@yahoo.co.uk>
2016-04-12 12:04:32 +0100

    Reverted sizes

--- make_sizes.py
+++ make_sizes.py
@@ -1,8 +1,7 @@
 for i in range(1, 9):
     print i
 
-for j in range(1, 14):
+for j in range(1, 13):
     for i in [5, 6, 7, 8]:
-        if i * 2**j < 65536:
-            print i * 2**j
-            print i * 2**j + 1
+        print i * 2**j
+        print i * 2**j + 1
