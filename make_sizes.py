b31ca5866c6e65a230e80bd2d23b0dbffc9fb5ee
jamestrimble <james.trimble@yahoo.co.uk>
2016-04-12 11:54:26 +0100

    Removed largest n

--- make_sizes.py
+++ make_sizes.py
@@ -3,5 +3,6 @@ for i in range(1, 9):
 
 for j in range(1, 14):
     for i in [5, 6, 7, 8]:
-        print i * 2**j
-        print i * 2**j + 1
+        if i * 2**j < 65536:
+            print i * 2**j
+            print i * 2**j + 1
